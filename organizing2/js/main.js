f3864bd44814380dfd4833c4f61cbe3079c4fcbe
Ahmed Alhosary <ahmedalhosary67@gmail.com>
2021-11-18 13:24:48 +0200

    update

--- organizing2/js/main.js
+++ organizing2/js/main.js
@@ -1,128 +1,163 @@
-// function togglePopup() {
-//   document.getElementById("popup-1").classList.toggle("active");
-// }
-let newModal = `<div class="custom-model-main">
-  <div class="container">
-    <div class="row">
-      <div class="col-12 col-md-8">
-        <div class="custom-model-inner">
-          <div class="custom-model-wrap">
-            <div class="pop-up-content-wrap">
-              <div class="items">
-                <div class="adding row">
-                  <div class="add col-6">Add Item</div>
-                  <div class="close-btn col-6">×</div>
-                </div>
-                <form >
-                  
-                  <div class="input-ele">
-                    <div class="mb-3 row">
-                      <label for="staticEmail" class="col-3 col-form-label">Clothe Name:</label>
-                      <div class="col-8">
-                        <input type="text" class="form-control clotheName" />
-                      </div>
-                    </div>
-                    <div class="mb-3 row">
-                      <label for="inputPassword" class="col-3 col-form-label">Clothe Color:</label>
-                      <div class="col-8">
-                        <input type="text" class="form-control clotheColor" />
-                      </div>
+// setTimeout(() => {
+//   $("#loader").fadeOut("slow");
+
+// }, 2000);
+$(this).on("keydown click", () => {
+  $("#loader").fadeOut("slow");
+  $(".welcome").addClass("animate__animated animate__fadeInDownBig");
+  $("table button").addClass("animate__animated animate__zoomIn");
+  $("header img").addClass("animate__animated animate__zoomIn");
+  $("section .export-pdf-button").addClass("animate__animated animate__fadeInLeftBig");
+});
+
+function openModel(type, e) {
+  document.getElementById("modal-wrapper").innerHTML += getModal(type);
+  $("#clothName").focus();
+}
+
+function closeModal() {
+  document.getElementById("modal-wrapper").innerHTML = "";
+}
+
+function addItem(type, event) {
+  event.preventDefault();
+
+  const clothName = document.getElementById("clothName");
+  const clothColor = document.getElementById("clothColor");
+  const clothType = document.getElementById("clothType");
+
+  const targetCategory = document.getElementById(clothType.value);
+  const table = targetCategory.getElementsByClassName(type)[0];
+
+  clothName.nextSibling.remove();
+
+  if (!clothName.value == "" || !clothColor.value == "") {
+    table.innerHTML += `<span class="task-box">${
+      clothName.value == "" ? "Undefind" : clothName.value
+    } _ ${clothColor.value == "" ? "Undefind" : clothColor.value}</span><br />`;
+
+    // if (clothName.value == "") {
+    //   clothName.parentNode.innerHTML += `<span class="text text-danger" style="position: absolute;font-size:20px">please insert cloth name</span>`;
+    //   // return;
+    // }
+    // if (clothColor.value == "") {
+    //   clothColor.parentNode.innerHTML += `<span class="text text-danger" style="position: absolute;font-size:20px">please insert cloth color</span>`;
+    // }
+
+    // return;
+  }
+
+  document.getElementById("modal-wrapper").innerHTML = "";
+}
+
+// Define the function
+// to screenshot the div
+function takeshot() {
+  let div = document.getElementById("table-container");
+  // Use the html2canvas
+  // function to take a screenshot
+  // and append it
+  // to the output div
+  $("#output").children().remove();
+  setTimeout(() => {
+    if ($("#output").parent().parent().parent().hasClass("show")) {
+      html2canvas(div).then(function (canvas) {
+        document.getElementById("output").appendChild(canvas);
+      });
+    }
+  }, 200);
+}
+
+function downloadPdf() {
+  var canvas = document.querySelector("canvas");
+  image = canvas
+    .toDataURL("image/png")
+    .replace("image/png", "image/octet-stream");
+  var link = document.createElement("a");
+  link.download = "my-image.png";
+  link.href = image;
+  link.click();
+  // $("#organizer").tableHTMLExport({ type: "pdf", filename: "organizer.pdf" });
+}
+
+function getModal(type) {
+  return `<div id="custom-model-main" class="custom-model-main model-open">
+<div class="container">
+  <div class="row">
+    <div class="col-12 col-md-8">
+      <div class="custom-model-inner">
+        <div class="custom-model-wrap">
+          <div class="pop-up-content-wrap">
+            <div class="items">
+              <div class="adding row">
+                <div class="add col-6">Add Item</div>
+                <div class="close-btn col-6" onclick="closeModal()">×</div>
+              </div>
+              <form onsubmit="addItem( '  ${type}  ', event)">
+                <div class="input-ele">
+                  <div class="mb-3 row">
+                    <label
+                      for="staticEmail"
+                      class="col-4 col-form-label"
+                      >Clothe Name:</label
+                    >
+                    <div class="col-7">
+                      <input
+                        id="clothName"
+                        type="text"
+                        class="form-control"
+                        min="1"
+                      />
                     </div>
                   </div>
-                  <button type="submit" class="btn btn-primary text-center submit">Save</button>
-                </form>
-              </div>
+                  <div class="mb-3 row">
+                    <label
+                      for="inputPassword"
+                      class="col-4 col-form-label"
+                      >Clothe Color:</label
+                    >
+                    <div class="col-7">
+                      <input
+                        id="clothColor"
+                        type="text"
+                        class="form-control"
+                        min="1"
+                      />
+                    </div>
+                  </div>
+                  <div class="mb-3 row">
+                    <label
+                      class="col-4 col-form-label"
+                      >Cloth type:</label
+                    >
+                    <div class="col-7">
+                      <select
+                        id="clothType"
+                        class="form-control"
+                      >
+                      <option value="workware">Workware</option>
+                      <option value="home">Home</option>
+                      <option value="sport">Sport</option>
+                      <option value="partyware">Partyware</option>
+                      </select>
+
+                    </div>
+                  </div>
+                </div>
+                <button
+                  id="saveData"
+                  class="btn btn-primary text-center submit"
+                >
+                  Save
+                </button>
+              </form>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="bg-overlay"></div>
-  </div>`;
-
-function downloadPdf() {
-  $("#organizer").tableHTMLExport({ type: "pdf", filename: "organizer.pdf" });
-  // $(".modal")
+</div>
+<div class="bg-overlay"  onclick="closeModal()"></div>
+</div>`;
 }
-
-// function addItem(e) {}
-$(".Click-here").on("click", function () {
-  $(".custom-model-main").addClass("model-open");
-  let saveButton = $(".submit"),
-    closeButton = $(".close-btn, .bg-overlay");
-  that = $(this),
-    td = $(this.parentNode);
-
-  // console.log(this.parentNode.classList.contains("notUsed"));
-  console.log($(this.parentNode).hasClass('bad'));
-
-  ////////////////// save button //////////////////////
-  saveButton.click((e) => {
-    e.preventDefault();
-
-    let clotheNameInput = $(".clotheName").val(),
-      clotheColorInput = $(".clotheColor").val(),
-      newTask = `<span class="task-box">${clotheNameInput} _ ${clotheColorInput}</span>`;
-
-    that.parent().append(newTask).addClass("filled");
-    that.remove();
-    $(".custom-model-main").remove();
-    $(".collect-parts").append(newModal);
-    let notUsed = $(".notUsed");
-    let bad = $(".bad");
-    let good = $(".good");
-
-    // console.log(this);
-    if (td.hasClass("notUsed")) {
-      console.log("notUsed");
-      for (let i = 0; i < notUsed.length; i++) {
-        const element = notUsed[i];
-        let itemButton = `<button class="Click-here" onclick="addItem(e)">+ Item</button>`;
-        // element.append(itemButton)
-        if (element.childNodes[0].classList.contains("hideItem")) {
-          element.childNodes[0].classList.remove("hideItem");
-          // element.innerHTML += itemButton;
-          break;
-        }
-      }
-    } else if (td.hasClass("bad")) {
-      console.log("bad");
-      for (let i = 0; i < bad.length; i++) {
-        const element = bad[i];
-        let itemButton = `<button class="Click-here" onclick="addItem(e)">+ Item</button>`;
-        // element.append(itemButton)
-        if (element.childNodes[0].classList.contains("hideItem")) {
-          element.childNodes[0].classList.remove("hideItem");
-          // element.innerHTML += itemButton;
-          break;
-        }
-      }
-    } else if (td.hasClass("good")) {
-      console.log("good");
-      for (let i = 0; i < good.length; i++) {
-        const element = good[i];
-        let itemButton = `<button class="Click-here" onclick="addItem(e)">+ Item</button>`;
-        // element.append(itemButton)
-        if (element.childNodes[0].classList.contains("hideItem")) {
-          element.childNodes[0].classList.remove("hideItem");
-          // element.innerHTML += itemButton;
-          break;
-        }
-      }
-    }
-    // clickHere += itemButton;
-  });
-  ////////////////// close button //////////////////////
-  closeButton.click((e) => {
-    // console.log("close");
-    $(".custom-model-main").removeClass("model-open");
-    $(".custom-model-main").remove();
-    $(".collect-parts").append(newModal);
-  });
-});
-
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus');
-  $('#myModal').modal('show')
-})
